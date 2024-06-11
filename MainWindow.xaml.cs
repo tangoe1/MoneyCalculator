c48ecde59f37fe8fb51a348535ef25a6310a4aeb
tangoe1 <tangoe2010.new@gmail.com>
2024-06-11 20:25:39 +0700

    Add the whole project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int income = int.Parse(txtIncome.Text);
+            int outcome = int.Parse(txtOutcome.Text);
+            int want = int.Parse(txtWant.Text);
+            double totalDay = Math.Ceiling(want / (income - outcome*1.0));
+            txtTotalDay.Text = totalDay.ToString();
+            
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
